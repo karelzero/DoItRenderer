e7f97278c3dded0bbe40668b4351d702aa865423
karelzero <karelzero@163.com>
2014-08-14 02:04:41 +0800

    add SharedPtr

---
+++ DoItRenderer/MemoryPool.h
@@ -0,0 +1,11 @@
+#ifndef __MEMORY_POOL_H__
+#define __MEMORY_POOL_H__
+
+namespace DIR
+{	
+	class MemoryPool
+	{
+	};
+}
+
+#endif
---
+++ DoItRenderer/SharedPtr.h
@@ -0,0 +1,92 @@
+#ifndef __SHARED_POINTER__
+#define __SHARED_POINTER__
+
+namespace DIR
+{
+	template<class T>
+	class SharedPtr
+	{
+	public:
+		SharedPtr(T* p) : m_pRef(NULL), m_pUsed(NULL)
+		{
+			if (p)
+			{
+				m_pRef = p;
+				m_pUsed = new unsigned int(1);
+				*m_pUsed = 1;
+			}
+		}
+
+		SharedPtr(const SharedPtr& sp) : m_pRef(NULL), m_pUsed(NULL)
+		{
+			Copy(sp);
+		}
+
+		~SharedPtr()
+		{
+			Release();
+		}
+
+		SharedPtr& operator=(const SharedPtr& sp)
+		{
+			if (m_pRef == sp.m_pRef)
+				return *this;
+
+			Copy(sp);
+			return *this;
+		}
+
+		void SetNull()
+		{
+			if (m_pRef)
+			{
+				Release();
+				m_pRef = NULL;
+				m_pUsed == NULL;
+			}
+		}
+
+		inline bool operator==(const SharedPtr& sp) { return m_pRef == sp.m_pRef; }
+		inline bool operator!=(const SharedPtr& sp) { return m_pRef != sp.m_pRef; }
+		inline T& operator*() { assert(m_pRef); return *m_pRef; }
+		inline T* operator->() { assert(m_pRef); return m_pRef; }
+
+		inline bool IsNull() { return m_pRef == NULL; }
+		inline T* GetPtr() const { return m_pRef; }
+		inline unsigned int GetUsed() const { assert(m_pUsed); return *m_pUsed; }
+		
+	protected:
+		void Copy(const SharedPtr& sp)
+		{
+			m_pRef = sp.m_pRef;
+			m_pUsed = sp.m_pUsed;
+			if (m_pUsed)
+				++(*m_pUsed);
+		}
+
+		void Release()
+		{
+			if (m_pUsed)
+			{
+				if (*m_pUsed > 0)
+					--(*m_pUsed);
+
+				if (*m_pUsed == 0)
+				{
+					if (m_pRef)
+						delete m_pRef;
+
+					delete m_pUsed;
+
+					m_pRef = NULL;
+					m_pUsed = NULL;
+				}
+			}
+		}
+
+	protected:
+		T* m_pRef;
+		unsigned int* m_pUsed;
+	};
+}
+#endif
